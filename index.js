63549bc95915a02e23a5e36a44db6ca08e0843d2
unknown <jamesjhn19@gmail.com>
2024-03-10 14:11:06 +0530

    second

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server')
 const MPServer = jsonServer.create()
 const middleware = jsonServer.defaults()
 const router = jsonServer.router('db.json')
-const PORT =3000 || process.env.PORT
+const PORT =4000 || process.env.PORT
 
 MPServer.use(middleware)
 MPServer.use(router)
